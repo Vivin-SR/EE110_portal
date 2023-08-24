bd840abe4252f2d741712497965f533ce0c3273e
Vivin-SR <107374442+Vivin-SR@users.noreply.github.com>
2023-08-24 16:08:33 +0530

    Add files via upload

---
+++ day5_portal/src/main/java/com/day5_portal/service/ApiService.java
@@ -0,0 +1,15 @@
+package com.day5_portal.service;
+
+import java.util.List;
+
+import com.day5_portal.models.Book;
+
+
+
+public interface ApiService {
+	
+
+public boolean adduser(Book book);
+public boolean updateuser(Long id, Book book);
+boolean deleteUser(Long id);
+}
