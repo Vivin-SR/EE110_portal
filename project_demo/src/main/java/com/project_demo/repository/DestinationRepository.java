7ecc647ad969b2ea4fc0b1cf9d67b9065b54f44d
Vivin-SR <107374442+Vivin-SR@users.noreply.github.com>
2023-09-01 10:04:34 +0530

    Delete project_demo directory

--- project_demo/src/main/java/com/project_demo/repository/DestinationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project_demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project_demo.model.Destinations;
-
-public interface DestinationRepository extends JpaRepository<Destinations,Integer>{
-
-}
--- project_demo/src/main/java/com/project_demo/repository/EnquiryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project_demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project_demo.model.Enquiry;
-
-public interface EnquiryRepository extends JpaRepository<Enquiry,Integer>{
-
-}
